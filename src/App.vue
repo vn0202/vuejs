64491ae7a8ade6c7d44ee5785c7ebd399cc3604a
vannghia <nghiavuxp0202@gmail.com>
2023-06-22 23:24:22 +0700

    composition fundamental

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,210 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref, computed } from 'vue'
+const header = ref('Shop list render')
+const lists = ref([]);
+const newItem = ref("");
+const newItemHightPriority =ref(false); 
+// I use destructuring here ..because I use lists.value.reverse() directly, the original lists will be change
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const reveserItems = computed(() => [...lists.value].reverse())
+const toggleStikeout = (item) =>{
+  item.strikeout = !item.strikeout;
+};
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const saveItem = ()=>{
+  lists.value.push(
+    {
+      id:lists.value.length,
+       label:newItem.value,
+       hightPriority: newItemHightPriority.value,
+       strikeout:true,
+      }
+    );
+  newItem.value = "";
+  newItemHightPriority.value =false;
+};
 </script>
-
+<template>
+  <h1>{{ header }}</h1>
+  <div class="add-item-form">
+    <input type="text" v-model.trim="newItem" placeholder="Enter new item">
+    <input type="checkbox" v-model="newItemHightPriority" id="hightPriority">
+    <label for="hightPriority">Hight priority</label>
+    <button :disabled="newItem.length < 5" v-on:click="saveItem" class="btn btn-primary">Add item </button>
+  </div>
+  <div>
+    <ul v-if="lists.length">
+      <li v-for="({ id, label,hightPriority, strikeout }, index) in reveserItems"
+       :key="id" 
+       :class="{'priority': hightPriority, 'strikeout': strikeout}"
+       @click = toggleStikeout(lists[index])
+       >{{ label }}</li>
+    </ul>
+    <p v-else>
+      Nothing is in list!
+    </p>
+  </div>
+  
+</template>
 <style>
+body {
+  background: #eff8ff;
+  height: 100vh;
+  width: 100vw;
+  font-family: system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto,
+    Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+}
+
+.counter {
+  font-size: 0.8rem;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #fff;
+  padding: 2rem;
+  margin: 1rem;
+  border-radius: 3px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+  width: 95%;
+  max-width: 900px;
+}
+
+h1 {
+  color: #3d4852;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+a {
+  color: #6cb2eb;
+  font-size: 1.25rem;
+  transition: all 0.1s ease-in;
+  margin-top: 0.5rem;
+  display: block;
+}
+
+a:hover {
+  color: #3490dc;
+}
+
+li,
+p {
+  display: flex;
+  align-items: center;
+  line-height: 1.75;
+  letter-spacing: 0.5px;
+  color: #3d4852;
+  font-size: 1.25rem;
+  cursor: pointer;
+  transition: all 0.1s ease-in;
+}
+
+li:hover {
+  color: #22292f;
+}
+
+li input {
+  margin: 0 0.5rem 0;
+}
+
+#shopping-list > input,
+#shopping-list > select {
+  width: 100%;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #f1f5f8;
+  color: #606f7b;
+  padding: 0.5rem 0.75rem;
+  box-sizing: border-box;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  margin: 0.5rem 0;
+}
+
+.add-item-form,
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.add-item-form input {
+  width: 70%;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #f1f5f8;
+  color: #606f7b;
+  padding: 0.5rem 0.75rem;
+  box-sizing: border-box;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  margin: 0.5rem 0;
+}
+
+.btn {
+  border: none;
+  border-radius: 3px;
+  margin: auto 0;
+  padding: 0.5rem 0.75rem;
+  flex-shrink: 0;
+  cursor: pointer;
+  font-size: 0.9rem;
+  letter-spacing: 0.5px;
+  transition: all 0.1s ease-in;
+}
+
+.btn[disabled] {
+  background: #8795a1;
+}
+
+.btn[disabled]:hover {
+  background: #606f7b;
+}
+
+.btn-primary {
+  background: #6cb2eb;
+  color: #fff;
+}
+
+.btn-primary:hover {
+  background: #3490dc;
+}
+
+.btn-cancel {
+  background: #ef5753;
+  color: #fff;
+}
+
+.btn-cancel:hover {
+  background: #e3342f;
+  color: #fff;
+}
+
+.strikeout {
+  text-decoration: line-through;
+  color: #b8c2cc;
+}
+
+.strikeout:hover {
+  color: #8795a1;
+}
+
+.priority {
+  color: #de751f;
+}
+input[type="checkbox"]{
+  display: inline !important;
+  box-shadow: none;
+  width: auto;
 }
 </style>
